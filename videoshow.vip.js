5cbbe941694879765bf2b9ccb71b9b8c2bf09368
LangKhach <langkhach270389@users.noreply.github.com>
2019-10-22 08:31:23 +0700

    update videoshow v1

--- videoshow.vip.js
+++ videoshow.vip.js
@@ -1,3 +1,15 @@
 var obj = JSON.parse($response.body);
-obj= { 	"original_transaction_id": "310000409847203", 	"product_id": "vip_privilege_monthly", 	"is_trial_period": "true", 	"status": "0", 	"current_date": "2019-05-02 13:15:02", 	"expires_date": "2020-04-29 01:25:28 Etc/GMT", 	"isBlacklist": "0", 	"cancellation_date": "", 	"purchase_date": "2019-04-26 01:25:28 Etc/GMT", 	"msg": "验证成功", 	"transaction_id": "310000409847203" };
+obj= {
+  "product_id": "vip_privilege_monthly",
+  "status": "0",
+  "isBlacklist": "0",
+  "transaction_id": "310000409847203",
+  "cancellation_date": "",
+  "expires_date": "2020-04-29 01:25:28 Etc/GMT",
+  "current_date": "2019-05-02 13:15:02",
+  "is_trial_period": "true",
+  "msg": "验证成功",
+  "original_transaction_id": "310000409847203",
+  "purchase_date": "2019-04-26 01:25:28 Etc/GMT"
+};
 $done({body: JSON.stringify(obj)});
