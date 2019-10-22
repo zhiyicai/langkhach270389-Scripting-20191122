38da84f3cb78c2c43d34a11ff76d9b6cff9319a3
LangKhach <langkhach270389@users.noreply.github.com>
2019-10-22 13:11:51 +0700

    fix 4g nhaccuatui.js

--- nhaccuatui.js
+++ nhaccuatui.js
@@ -1,5 +1,9 @@
 var obj = JSON.parse($response.body);
-
+let url = $request.url;
+var cons = "v6/users/info";
+if(url.indexOf(cons) != -1)
+{
 obj.data.VIPExpire= "27/03/9999 00:00:00";
 obj.data.isVIP= true;
+}
 $done({body: JSON.stringify(obj)});
