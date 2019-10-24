17cec003e3ecd699042c9d58eb9c13aa63e783c2
LangKhach <langkhach270389@users.noreply.github.com>
2019-10-24 15:10:17 +0700

    fix code zingtvv1

--- zingtvvipv1.js
+++ zingtvvipv1.js
@@ -3,6 +3,6 @@ var url = $request.url;
 var cons = "user/info";
 if(url.indexOf(cons) != -1)
 {
-obj["data"]["premium"]["is_premium] = true;
+obj["data"]["premium"]["is_premium"] = true;
 }
 $done({body: JSON.stringify(obj)});
