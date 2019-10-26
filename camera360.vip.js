09330e68c92c33824efa288c46e4b26e351f623a
LangKhach <langkhach270389@users.noreply.github.com>
2019-10-26 09:22:56 +0700

    fix camera 360

--- camera360.vip.js
+++ camera360.vip.js
@@ -1,19 +1,19 @@
 var obj = JSON.parse($response.body);
 obj=
 {
- "data": {
-  "errorCode": 0,
-  "sandbox": 0,
-  "purchaseTime": 1571682188,
-  "expireTime": 4096532852,
-  "productId": "VIP_yearly_29.99",
-  "appleVip": 1,
-  "operationVip": 0,
-  "giftVip": 0
- },
- "status": 200,
- "message": "ok",
- "serverTime": 157192473.9075,
- "exetime": "1571924739075-4096532852866"
+  "status": 200,
+  "data": {
+    "sandbox": 1,
+    "purchaseTime": 1571682188,
+    "giftVip": 0,
+    "productId": "VIP_yearly_29.99",
+    "appleVip": 1,
+    "expireTime": 4096532852,
+    "operationVip": 0,
+    "errorCode": 0
+  },
+  "message": "ok",
+  "exetime": "1572056389212-1572056391362",
+  "serverTime": 1572056391.3622
 };
 $done({body: JSON.stringify(obj)});
