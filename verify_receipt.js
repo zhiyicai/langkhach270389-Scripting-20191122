6e87c7347fa7e08cf18cd0607c6f37aa5fec428c
Lãng Khách <langkhach270389@users.noreply.github.com>
2019-10-30 08:45:43 +0700

    qxv1.02

--- verify_receipt.js
+++ verify_receipt.js
@@ -5,5 +5,5 @@ let obj = JSON.parse($response.body);
   obj["latest_receipt_info"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
   
   obj["latest_receipt_info"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
-obj["latest_receipt_info"][0]["expires_date_ms"] = "4116323658000";
+  obj["latest_receipt_info"][0]["expires_date_ms"] = "4116323658000";
 $done({body: JSON.stringify(obj)});
