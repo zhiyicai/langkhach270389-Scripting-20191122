146b8fc1ebf1164eba86fc4b5bbe57c06355648d
Lãng Khách <langkhach270389@users.noreply.github.com>
2019-10-31 14:54:29 +0700

    fix memrise.vip.js js errors notify

--- memrise.vip.js
+++ memrise.vip.js
@@ -1,13 +1,7 @@
 let url = $request.url;
 let obj= JSON.parse($response.body);
-var cons1 = "me";
 var cons2 = "dashboard";
 var cons3 = "leaderboard";
-if(url.indexOf(cons1) != -1)
-{
-obj.profile["is_pro"]= true;
-body= JSON.stringify(obj);
-}
 if(url.indexOf(cons2) != -1)
 {
 obj["user"].is_premium= true;
