1dcedd4ebf039925e51cc9b86e821053eb6f81bb
Lãng Khách <langkhach270389@users.noreply.github.com>
2019-10-31 12:05:18 +0700

    mojo test

--- mojo.vip.js
+++ mojo.vip.js
@@ -1,11 +1,11 @@
 var obj = JSON.parse($response.body);
 
 obj= {
-  "request_date_ms": 1564945639879,
-  "request_date": "2019-08-04T19:07:19Z",
+  "request_date_ms": 1572490296249,
+  "request_date": "2019-10-31T02:51:36Z",
   "subscriber": {
-    "first_seen": "2019-08-04T19:03:30Z",
-    "original_application_version": "169",
+    "first_seen": "2019-10-12T00:37:49Z",
+    "original_application_version": "194",
     "other_purchases": {
     },
     "subscriptions": {
@@ -14,20 +14,21 @@ obj= {
         "period_type": "trial",
         "billing_issues_detected_at": null,
         "unsubscribe_detected_at": null,
-        "expires_date": "2099-09-04T19:06:30Z",
-        "original_purchase_date": "2019-08-04T19:06:30Z",
-        "purchase_date": "2019-08-04T19:06:30Z",
+        "expires_date": "2099-12-01T03:51:32Z",
+        "original_purchase_date": "2019-10-31T02:51:33Z",
+        "purchase_date": "2019-10-31T02:51:32Z",
         "store": "app_store"
       }
     },
     "entitlements": {
       "pro": {
-        "expires_date": "2019-09-04T19:06:30Z",
+        "expires_date": "2099-12-01T03:51:32Z",
         "product_identifier": "video.mojo.pro.yearly",
-        "purchase_date": "2019-08-04T19:06:30Z"
+        "purchase_date": "2019-10-31T02:51:32Z"
       }
     },
-    "original_app_user_id": "iiVz37G5i9az15Vfr5lskGOvhkf1",
+    "original_purchase_date": "2019-10-12T00:03:39Z",
+    "original_app_user_id": "lTNT8zWozfXUzstQM02ZqqkoSh03",
     "non_subscriptions": {
     }
   }
--- verify_receipt.js
+++ verify_receipt.js
@@ -1,10 +1,12 @@
-let obj = JSON.parse($response.body);
-  obj["receipt"]["in_app"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
-  obj["receipt"]["in_app"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
-  obj["receipt"]["in_app"][0]["expires_date_ms"] = "4116323658000";
-  obj["latest_receipt_info"][0]["expires_date"] = "2099-10-19 05:14:18 Etc/GMT";
-  
-  obj["latest_receipt_info"][0]["expires_date_pst"] = "2099-10-18 22:14:18 America/Los_Angeles";
-  obj["latest_receipt_info"][0]["expires_date_ms"] = "4116323658000";
+var obj = JSON.parse($response.body);
+if(obj.status == 0)
+{
+obj.receipt.in_app[0].expires_date = "2099-05-02 06:35:04 Etc/GMT",
+obj.receipt.in_app[0].expires_date_pst = "2099-05-01 23:35:04 America/Los_Angeles";
+obj.receipt.in_app[0].expires_date_ms = "4081386904000";
+obj.latest_receipt_info[0].expires_date = "2099-05-02 06:35:04 Etc/GMT";
+obj.latest_receipt_info[0].expires_date_pst = "2099-05-01 23:35:04 America/Los_Angeles";
+obj.latest_receipt_info[0].expires_date_ms = "4081386904000";
+}
 $done({body: JSON.stringify(obj)});
 //Magi+, kunicam, kinemaster, filmr, intromaker, snell, gemini, Manly
