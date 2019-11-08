1eb31b2425a220758ee857c2fdb9c30b99715b47
Lãng Khách <langkhach270389@users.noreply.github.com>
2019-11-08 14:53:01 +0700

    zingmp3 test

---
+++ zingmp3.js
@@ -0,0 +1,7 @@
+body = $response.body.replace(/"lastVipExpireTime".*\d{10}\n/, "");
+var obj = JSON.parse(body);
+obj["data"]["userId"]=1002671088;
+obj["data"]["sessionsKey"]= "sm1.mfc_.1009719336.10.vjnZ26bVD1L1HnifQLyq5MbVD1LfNpGLQDphhpHVD1K";
+obj["data"]["vip"]= {"expireTime":1869254270,"startTime":1572527803,"subscription":{"status":1,"expireTime": 1869254270,"platform":2},"vipType":1};
+});
+$done({body: JSON.stringify(obj)});
