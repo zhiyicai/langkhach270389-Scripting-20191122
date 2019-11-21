14b059deed97a39b23692adc1f3c93844c073a34
Lãng Khách <langkhach270389@users.noreply.github.com>
2019-11-21 11:11:49 +0700

    fix date

--- draft.js
+++ draft.js
@@ -1,7 +1,7 @@
 var obj = JSON.parse($response.body);
 
 obj= {
-  "active_expires_at" : "2099-01-01T00:00:00Z",
+  "active_expires_at" : "2029-01-01T00:00:00Z",
   "is_subscription_active" : true,
   "active_subscription_type" : "premium",
   "is_blocked" : false
